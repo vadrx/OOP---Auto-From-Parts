d0306b5714bb486f84470c4ba7df8864d13c34d3
Anton <vadrx@yandex.ru>
2017-12-22 12:53:09 +0300

    Initial Commit

---
+++ lab3OOP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab3OOP
+//
+//  Created by Антон Волков on 22.12.2017.
+//  Copyright © 2017 Антон Волков. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
